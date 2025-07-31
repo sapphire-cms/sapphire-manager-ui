e4c6626a532f95c966b8fb067ea282ad517569a9
Alexei KLENIN <alexei.klenin@gmail.com>
2025-08-01 00:22:35 +0200

    FIX: resizing of Monaco editor

--- src/app/default-inputs/rich-text-input/raw/raw.component.ts
+++ src/app/default-inputs/rich-text-input/raw/raw.component.ts
@@ -1,5 +1,7 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, ViewChild} from '@angular/core';
 import {AbstractInput} from '../../../inputs-base/abstract-input';
+import * as monaco from 'monaco-editor';
+import {EditorComponent} from 'ngx-monaco-editor-v2';
 
 @Component({
   selector: 'scms-raw',
@@ -11,12 +13,38 @@ export class RawComponent extends AbstractInput<string> {
   protected readonly editorOptions = {
     language: 'markdown',
     theme: 'vs',
-    automaticLayout: true,
     lineNumbers: 'off',
     minimap: {
       enabled: false,
     },
     scrollBeyondLastLine: false,
+    automaticLayout: false,
     overviewRulerLanes: 0,
   };
+
+  protected containerHeight = '100px';
+
+  @ViewChild('editorComponent', { static: true })
+  private readonly editorComponent!: EditorComponent;
+
+  private editor!: monaco.editor.IStandaloneCodeEditor;
+
+  constructor(private readonly cdr: ChangeDetectorRef) {
+    super();
+  }
+
+  protected onEditorInit(editor: monaco.editor.IStandaloneCodeEditor): void {
+    this.editor = editor;
+    editor.onDidContentSizeChange(() => this.resizeToContent());
+  }
+
+  private resizeToContent(): void {
+    const height = Math.min(600, Math.max(100, this.editor.getContentHeight()));
+
+    this.containerHeight = `${height + 24}px`; // 24 pixels for border and padding of textarea
+    this.editorComponent._editorContainer.nativeElement.style.height = `${height}px`;
+
+    this.cdr.markForCheck();
+    this.editor.layout();
+  }
 }
